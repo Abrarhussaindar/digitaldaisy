548839ced472fdbe427941dc569f9394acf341d6
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2023-08-10 16:13:50 +0530

    changed " "

--- login_system/urls.py
+++ login_system/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('login/', views.user_login, name="login"),
+    path('home/', views.home, name="home"),
+    path('', views.user_login, name="login"),
     path('logout/', views.user_logout, name="user_logout"),
     path('register/', views.create_new_user, name="register"),
     path('get_work_time/', views.get_work_time, name='get_work_time'),
